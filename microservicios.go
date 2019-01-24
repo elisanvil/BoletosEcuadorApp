28f0c7cbc195b92fcbca928c29ad77b19c752a2f
Ubuntu <ubuntu@ip-172-31-54-115.ec2.internal>
2019-01-24 07:39:15 +0000

    actualizacion de microserv

--- microservicios.go
+++ microservicios.go
@@ -10,22 +10,22 @@ import (
 
 
 func main() {
-	db, err := sql.Open("mysql", "root:Ragderom1924@/local_boletos")
+	db, err := sql.Open("mysql", "loor:Rxlz_01@@/boletosecuador")
 	if err != nil {
 		panic(err.Error())  // Just for example purpose. You should use proper error handling instead of panic
 	}
 	defer db.Close()
 
 	// Prepare statement for inserting data
-	/*stmtIns, err := db.Prepare("INSERT INTO evento (id, name, hora, id_venue, fecha) VALUES(null, ?, NOW(), ?, CURDATE())") // ? = placeholder
+	stmtIns, err := db.Prepare("INSERT INTO evento (id_evento, nombre, hora, id_venue, fecha) VALUES(null, ?, NOW(), ?, CURDATE())") // ? = placeholder
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
-	defer stmtIns.Close() // Close the statement when we leave main() / the program terminates*/
+	defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
 
 	// Prepare statement for reading data
 	//Usuario pueden ver eventos disponibles
-	stmtOut, err := db.Prepare("SELECT * FROM evento WHERE id = ?")
+	stmtOut, err := db.Prepare("SELECT * FROM evento WHERE id_evento = ?")
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
@@ -37,27 +37,28 @@ func main() {
 	}
 	defer stmtOut2.Close()
 
-	/*names := [4]string{"Funkafest", "Shakira", "Chayanne", "Barcelona vs. Macara"}
+	names := [4]string{"Funkafest", "Shakira", "Chayanne", "Barcelona vs. Macara"}
 	for index, element := range names{
 		_, err = stmtIns.Exec(element, index)
 		if err != nil {
 			panic(err.Error())
 		}
-	}*/
+	}
 
 	var id int
 	var name string
 	var hora string
 	var id_venue int
 	var fecha string
+	var tipo string
 
 	var ctx = context.Background()
 
-	err = stmtOut.QueryRow(1).Scan(&id, &name, &hora, &id_venue, &fecha)
+	err = stmtOut.QueryRow(1).Scan(&id, &name, &tipo, &id_venue, &fecha, &hora)
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
-	fmt.Printf("id %d name %s hora %s id_venue %d fecha %s\n", id, name, hora, id_venue, fecha)
+	fmt.Printf("id %d name %s tipo %s id_venue %d fecha %s hora %s\n", id, name, tipo, id_venue, fecha, hora)
 
 	
 	rows, err := db.QueryContext(ctx, "SELECT * FROM evento")
@@ -67,11 +68,11 @@ func main() {
 	defer rows.Close()
 	ids := make([]int, 0)
 	for rows.Next(){
-		if err := rows.Scan(&id, &name, &hora, &id_venue, &fecha); err != nil {
+		if err := rows.Scan(&id, &name, &tipo, &id_venue, &fecha, &hora); err != nil {
 			panic(err.Error())
 		}
 		ids = append(ids, id)
-		fmt.Printf("id %d name %s hora %s id_venue %d fecha %s\n", id, name, hora, id_venue, fecha)
+		fmt.Printf("id %d name %s tipo %s id_venue %d fecha %s hora %s\n", id, name, tipo, id_venue, fecha, hora)
 	}
 	if err := rows.Err(); err != nil {
 		panic(err.Error())
@@ -79,7 +80,8 @@ func main() {
 	fmt.Printf("ids obtenidos %d", ids)
 
 	//creacion de venues
-	stmtIns2, err := db.Prepare("INSERT INTO venue (id_venue, name, ubicacion, tipo, capacidad, disponibilidad) VALUES (null, ?, ?, ?, ?, ?)")
+
+	stmtIns2, err := db.Prepare("INSERT INTO venue (id_venue, nombre, ubicacion, tipo, capacidad, disponibilidad) VALUES (null, ?, ?, ?, ?, ?)")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -88,13 +90,10 @@ func main() {
 	venue_names := [3]string{"Cto. convenciones", "Casa de la Cultura", "Coliseo Voltaire"}
 	venue_ubicacion := [3]string{"Guayaquil", "Quito", "Guayaquil"}
 	venue_tipo := [3]string{"Teatro", "Agora", "Coliseo"}
-	venue_capacidad := [3]int{10000, 55000, 1000000}
-	//fmt.Printf(venue_names[0])
-	//fmt.Printf(venue_ubicacion[1])
-	//fmt.Printf(venue_tipo[2])
-	//fmt.Printf(venue_capacidad[1])
+	venue_capacidad := [3]string{"10000", "55000", "1000000"}
+
 	for index, element := range venue_names{
-		_, err = stmtIns2.Exec(element, venue_ubicacion[index], venue_tipo[index], venue_capacidad[index], 1)
+		_, err = stmtIns2.Exec(element, venue_ubicacion[index], venue_tipo[index], venue_capacidad[index], "disponible")
 		if err != nil {
 			panic(err.Error())
 		}
